972ad5e438b4e45e7b3a9d18af7205a58068318d
Adam WECKER <azqe31@gmail.com>
2023-05-04 10:35:08 +0200

    pytest + workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="monotonic derivative",
-    version="0.13",
+    version="0.14",
     description="Monotonic Derivative is a Python library designed to modify real-life data to ensure that the specified degree derivative of the cubic spline is always monotonically increasing or decreasing. This library can be particularly useful in applications where the derivatives of the given data must follow specific monotonicity constraints, such as in scientific modeling or engineering applications.",
     author="Adam Wecker",
     packages=["monotonic_derivative"],
