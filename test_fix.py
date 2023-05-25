2e29a47a17672de2d84dd3d7a26f92c9a1ba2808
Adam WECKER <azqe31@gmail.com>
2023-05-25 12:55:43 +0200

    rework some test

--- test_fix.py
+++ test_fix.py
@@ -4,22 +4,14 @@ from monotonic_derivative.monotonic_derivative import (
 from monotonic_derivative.curve_smoothing import curve_smoothing
 import numpy as np
 
-y = np.array([1, 1, 3.5, 5, 6, 6.1, 4, 5])
-x = np.arange(len(y))
+x = np.array([0, 1, 2, 3, 4, 5])
+y = np.array([100, 55, 53, 40, 35, 5])
 
-
-y_ensure = ensure_monotonic_derivative(
-    x=np.arange(len(y)),
-    y=y,
-    degree=2,
-    force_negative_derivative=True,
-    verbose=True,
-    save_plot=True,
-)
-y_smooth = curve_smoothing(
+print(ensure_monotonic_derivative(x, y, degree=6))
+"""y_smooth = curve_smoothing(
     y_ensure, population_size=100, num_generations=1000, alpha=0.7, save_plots=True
 )
-print("smooth", y_smooth)
+print("smooth", y_smooth)"""
 """
 y_ensure2 = ensure_monotonic_derivative(
     x=np.arange(len(y_smooth)),
